e83a09987455f996f29f8c5263040699da8f1e1e
Tomas Glabazna <tomas@glabazna.cz>
2023-01-19 21:35:38 +0100

    January 19 commit

--- src/views/ProjectDetailPage.vue
+++ src/views/ProjectDetailPage.vue
@@ -1,11 +1,46 @@
 <template>
-  <h1>project detail</h1>
+  <div>
+    <t-loading v-if="loading" />
+    <template v-else>
+      <h1>{{ project.project }}</h1>
+    </template>
+  </div>
+
 </template>
 
 <script>
-export default {
 
-  name: 'ProjectDetailPage'
+import db from '../helpers/db.js'
+import TLoading from '../components/TLoading.vue'
+
+export default {
+  name: 'ProjectDetailPage',
+  data () {
+    return {
+      project: null,
+      loading: true
+    }
+  },
+  computed: {
+    projectid () {
+      return this.$route.params.id
+    }
+  },
+  created () {
+    db.get('js4projects/' + this.projectid).then((record) => {
+      this.project = record
+      this.loading = false
+    })
+    db.get('js4tasks?projectid=' + this.projectid).then((tasks) => {
+      console.log(tasks)
+    })
+    //   this.project = data.find(record => {
+    //     return record.id === this.$route.params.id
+    //   })
+    // })
+  },
+  components: { TLoading }
+
 }
 
 </script>
